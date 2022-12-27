f8b6c3721a9652b196a03e5cbf446d2e504a35d3
Ludumo <ludumo.toni@outlook.com>
2022-12-27 15:46:52 +0100

    text change

--- components/About.jsx
+++ components/About.jsx
@@ -12,7 +12,7 @@ const About = ({heading, message}) => {
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/20 z-[2]' />
       <div className=' min-w-[0] min-h-[0] z-[2] mt-[-15]'>
         <h2 className='text-5xl text-white font-light '>About</h2>
-        <p className='flex-row  w-64 h-64 pb-m64 py-5 text-xl  text-s text-wrap  text-white'>
+        <p className='flex-row  w-64 h-64 pb-m64 py-5 text-l  text-s text-wrap  text-white'>
         We understand the challenges and frustrations people face when it comes to housing. 
         We offer high-quality homes that can be constructed and erected quickly, without unnecessary bureaucracy. 
         We aim to make the process of finding and securing a new home as stress-free as possible, 
