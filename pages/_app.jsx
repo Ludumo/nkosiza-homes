5127b7d8475d820bd50164696ba6eefb53eab0fa
Ludumo <ludumo.toni@outlook.com>
2023-02-07 12:51:59 +0100

    mobile optimization

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,40 +1,16 @@
-import { motion ,AnimatePresence } from 'framer-motion';
+
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
-import { useRouter } from 'next/router';
+
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
   return (
-  <AnimatePresence mode="wait">        
-    <motion.div
-      key={router.route}
-      initial="initialState"
-      animate="animateState"
-      exit="exitState"
-      transition={{
-        duration: 0.75,
-      }}
-      variants={{
-        initialState:{
-          opacity: 0,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", 
-        }, 
-        animateState: {
-          opacity: 1,
-          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", 
-      }, 
-      exitState:{
-        clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)", 
-      }
-    }}
-    className="base-page-sizes">
+    <div>
       <Navbar  />
       <Component  {...pageProps} />
       <Footer />
-    </motion.div>
-  </AnimatePresence>
+    </div>
   );
 }
 
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,31 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
-
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                var script = document.createElement('script');
-                script.src = 'https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.js';
-                document.head.appendChild(script);
-              })();
-            `,
-          }}
-        />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
           href='https://fonts.gstatic.com'
           crossOrigin='true'
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:opsz,wght@8..144,100&display=swap" rel="stylesheet"/>
-        <link href="https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.css" rel="stylesheet" />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <body>
         <Main />
