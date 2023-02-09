7dc42f085a1cf56bb7240fee505201970fcab332
Ludumo <ludumo.toni@outlook.com>
2023-02-09 09:54:38 +0100

    image optimization

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,41 @@
-
+import { motion ,AnimatePresence } from 'framer-motion';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
-
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
-    <div>
+  <AnimatePresence mode="wait">        
+    <motion.div
+      key={router.route}
+      initial="initialState"
+      animate="animateState"
+      exit="exitState"
+      transition={{
+        duration: 0.75,
+      }}
+      variants={{
+        initialState:{
+          opacity: 0,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", 
+        }, 
+        animateState: {
+          opacity: 1,
+          clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)", 
+      }, 
+      exitState:{
+        clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)", 
+      }
+    }}
+    className="base-page-sizes">
+        
       <Navbar  />
       <Component  {...pageProps} />
       <Footer />
-    </div>
+    </motion.div>
+  </AnimatePresence>
   );
 }
 
